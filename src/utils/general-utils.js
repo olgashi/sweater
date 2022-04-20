796abf8eb55617d6fa319beed472cf4bd3addce8
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-04-20 12:34:24 -0500

    Refactor NextDaysContainer (extracted functions to separate file, reduced overall code complexity)

--- src/utils/general-utils.js
+++ src/utils/general-utils.js
@@ -1,3 +1,7 @@
+import { INCHES_IN_MM } from './const-utils';
+import { convertToWeekDayShort } from './date-utils';
+import { determineUVSevereness } from './text-utils';
+
 const moment = require('moment');
 require('moment-timezone');
 
@@ -17,7 +21,6 @@ export function getAlerts(weatherData) {
   if (weatherData.alerts) {
     return weatherData.alerts.map(alert => alert.description).filter(el => el).join(', ')
   }
-
   return null
 }
 
@@ -31,4 +34,32 @@ export function getLocationLookupDataFromInput(elementId) {
   const country = inputElement.getAttribute('country');
 
   return { userSearchInput, lat, lon, city, region, country };
+}
+
+export function determinePercipitation(dayDataObj) {
+  let percipitation = null;
+  if (dayDataObj.hasOwnProperty('snow')) {
+    percipitation = { type: 'Snow', value: (parseFloat(dayDataObj.snow) * INCHES_IN_MM).toFixed(2)};
+  } else if (dayDataObj.hasOwnProperty('rain')) {
+    percipitation =  { type: 'Rain', value: (parseFloat(dayDataObj.rain) * INCHES_IN_MM).toFixed(2)};
+  }
+  return percipitation;
+}
+
+export function generateDailyWeatherDataObj(dayDataObj, timezone) {
+  return {
+    day: convertToWeekDayShort(dayDataObj.dt, timezone),
+    description: dayDataObj.weather[0].description,
+    tempLow: Math.round(dayDataObj.temp.min),
+    tempHigh: Math.round(dayDataObj.temp.max),
+    iconUrl: `http://openweathermap.org/img/wn/${dayDataObj.weather[0].icon}.png`,
+    feelsLikeTemp: dayDataObj.feels_like.day, // TODO should be assigned conditionally based on the time of the day
+    humidity: `${dayDataObj.humidity}%`,
+    sunrise: moment.unix(dayDataObj.sunrise).format('h:mm a'),
+    sunset: moment.unix(dayDataObj.sunset).format('h:mm a'),
+    wind: Math.round(dayDataObj.wind_speed),
+    cloudness: `${dayDataObj.clouds}%`,
+    uvIndex: `${dayDataObj.uvi} - ${determineUVSevereness(dayDataObj.uvi)}`,
+    percipitation: determinePercipitation(dayDataObj),
+  };
 }
