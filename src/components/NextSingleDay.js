f5a7416a209e64543cb36ceadf83f30385c47046
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-04-19 21:38:32 -0500

    Refactor

--- src/components/NextSingleDay.js
+++ src/components/NextSingleDay.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Row, Col, Image } from 'react-bootstrap';
-import { allWordsWoUpper } from '../utils/text-utils';
+import { allWordsToUpper } from '../utils/text-utils';
 
 export default function NextSingleDay (props) {
   const {
@@ -11,12 +11,12 @@ export default function NextSingleDay (props) {
     } = props;
   return (
       <Row className="single-day-weather">
-        <Col md={2}>
+        <Col md={3}>
         <Row className="next-day-weekday">
         {day}
         </Row>
         <Row className="next-day-description">
-        {allWordsWoUpper(description)}
+        {allWordsToUpper(description)}
         </Row>
         </Col>
 
@@ -26,12 +26,12 @@ export default function NextSingleDay (props) {
         <Col md={3}>
 
         <Row className="next-day-low-high">
-        low/high:
+        High/Low:
         </Row>
 
         <Row className="next-day-low-high-temp">
-        {Math.round(tempLow)}/
-        {Math.round(tempHigh)} F
+        {Math.round(tempHigh)}/
+        {Math.round(tempLow)}F
         </Row>
         <Row className="next-day-feels-like">Feels like: {feelsLikeTemp}</Row>
         {percipitation ? <>
@@ -42,8 +42,7 @@ export default function NextSingleDay (props) {
 
         </Col>
       <Col md={1}>
-        <Row className="next-day-sunset-icon">
-        sunset:         
+        <Row className="next-day-sunset-icon">      
         <div>
           <img src={require('../images/sunset.png')}></img>
         </div>
@@ -55,7 +54,6 @@ export default function NextSingleDay (props) {
     
         <Col md={1}>
         <Row className="next-day-sunrise-icon">
-        sunrise:
         <div>
           <img src={require('../images/sunrise.png')}></img>
         </div>
@@ -66,7 +64,6 @@ export default function NextSingleDay (props) {
         </Row>
 
         </Col>
-        <Col md={1}></Col>
         <Col md={3}>
           <Row className="next-day-humidity">Humidity: {humidity}</Row>
           <Row className="next-day-wind">Wind: {wind} mph</Row>
@@ -76,19 +73,3 @@ export default function NextSingleDay (props) {
       </Row>
   )
 }
-
-// NextSingleDay.propTypes = {
-//   day: PropTypes.string, 
-//   description: PropTypes.string, 
-//   tempLow: PropTypes.string, 
-//   tempHigh: PropTypes.string, 
-//   iconUrl: PropTypes.string, 
-//   feelsLikeTemp: PropTypes.string,
-//   humidity: PropTypes.string,
-//   sunrise: PropTypes.string,
-//   sunset: PropTypes.string,
-//   percipitation: PropTypes.string,
-//   wind: PropTypes.string,
-//   cloudness: PropTypes.string,
-//   uvIndex: PropTypes.string,
-// }
