52a03d0652c89b7660cab639b3179688be26e817
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-04-20 13:15:59 -0500

    Add test for Next 5 days header

---
+++ src/components/NextHeader.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import NextHeader from './NextHeader'
+
+test('renders Next header', () => {
+  render(<NextHeader timeRangeAmount="5" timeRange="days"/>)
+
+  const element = screen.getByText('Next 5 days')
+  expect(element).toBeDefined()
+})
