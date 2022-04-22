cf450343da77f9353241fa413d2cc2fe641afc90
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-04-22 12:11:45 -0500

    Correct value

--- src/utils/const-utils.js
+++ src/utils/const-utils.js
@@ -3,4 +3,4 @@ export const CACHE_EXPIRATION_MINUTES = 10;
 export const NUM_DAYS_TO_DISPLAY = 7;
 export const NUM_HOURS_TO_DISPLAY = 5;
 export const UNIT_OF_DURATION_DAYS = "days";
-export const UNIT_OF_DURATION_HOURS = "days";
+export const UNIT_OF_DURATION_HOURS = "hours";
